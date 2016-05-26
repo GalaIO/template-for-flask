b45f4ef54233d86ce3e26a200b186eba8ed8e83b
GalaIO <505421435@qq.com>
2016-05-26 22:54:27 +0800

    测试2

--- app/main/views.py
+++ app/main/views.py
@@ -2,18 +2,26 @@
 '''
 index route.
 '''
-from flask import render_template, make_response
+from flask import render_template, make_response, request
 from . import main
 from ..models import System
+from ..models import db
 
 # 定义路由函数
 @main.route('/', methods=['GET', 'POST'])
 def index_route():
-    tmp = System.query.filter_by(name='xiaoming').first()
+    return render_template('index.html', name='anomymous')
+
+@main.route('/test/<name>', methods=['GET'])
+def test(name):
+    tmp = System.query.filter_by(name=name).first()
     if tmp is None:
         return render_template('index.html', name='anomymous')
     return render_template('index.html', name=tmp.name)
 
-@main.route('/test', methods=['GET'])
-def test():
-    return make_response('hhhh')
+@main.route('/new', methods=['GET'])
+def new():
+    name = request.args.get('name')
+    tmp = System(name=name)
+    db.session.add(tmp)
+    return make_response('success')
